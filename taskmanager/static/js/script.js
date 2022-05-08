c120ce64d0d7aa7e998590d0253abdc29af83537
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-05-08 15:35:27 +0000

    Add read functionality to tasks page

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidebar navigation
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  // sidebar navigation
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  var datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"}
   });
+
+  var selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
+  // Collapsiblies
+
+  var collapsibles = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsibles);
+});
